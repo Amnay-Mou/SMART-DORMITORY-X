50dea25e9435c88e872b1cfc6054d5b079e01593
Amnay-Mou <112958107+Amnay-Mou@users.noreply.github.com>
2025-05-13 12:28:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -560,7 +560,7 @@ def register():
 
 
 # --------------------------LOGIN-------------------------------
-@app.route('/', methods=['GET', 'POST']) # login
+@app.route('/login', methods=['GET', 'POST']) # login
 def login():
     info = None
     error = None
